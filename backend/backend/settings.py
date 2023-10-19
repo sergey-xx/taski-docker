ec21ea8aed5931eefb3d5704053fd3dccbcd64c9
Sergey Mazilin <mazilin.sergey@yandex.ru>
2023-10-19 22:22:15 +0400

    add debug to env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG') in ['TRUE', 'true', '1', 'yes']
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
